8dfbc5851ec3aeed7034eced28d737cfea891d0e
Huong Dang <miley.dawn@codeimmersives.com>
2020-06-24 01:30:41 -0400

    2nd time commit

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,9 +1,24 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate = require('./calculate-back-end.js')
 
+let num1 = getInput(0)
+let num2 = getInput(2)
+let operation = getInput(1)
 
+function userInput() {
+  
+  if (getInput(3)) {
+     operation = getInput(1) + ' ' + getInput(2)
+     num2 = getInput(3)
+     return calculate(num1, num2, operation)
+  } else if (!getInput(3)) {
+    return calculate(num1, num2, operation)
+  }
+}
 
+console.log(userInput(num1, num2, operation))
 
 
 
